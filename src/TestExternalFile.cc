5a3d1da15ed595f30c6b3dbc14e4b3bd071c6295
Stanley Fujimoto <sfujimoto@gmail.com>
2018-10-04 15:22:45 -0600

    updated external test
    now checks contains as well as insertion

--- src/TestExternalFile.cc
+++ src/TestExternalFile.cc
@@ -7,17 +7,17 @@
 
 int main( int argc, char* argv[] )
 {
+    int c = 0, interval = 100000;
     int k = atoi( argv[ 1 ] );
     Kdict* kd = new Kdict( k );
-    std::ifstream infile( argv[ 2 ] );
-
-    int c = 0;
-    std::string line;
     char* kmer = ( char* ) malloc( sizeof( char ) * ( k + 1 ) );
-    while (std::getline(infile, line))
+   
+    std::cout << "Inserting kmers";
+    std::ifstream infile( argv[ 2 ] );
+    std::string line;
+    while( std::getline( infile, line ) )
     {
         std::istringstream iss(line);
-        //std::cout << line.substr( 0, k ) << std::endl;
         strcpy( kmer, line.substr( 0, k ).c_str() );
 
         kd->insert( kmer );
@@ -26,10 +26,32 @@ int main( int argc, char* argv[] )
             std::cout << "True!" << std::endl;
         else
             std::cout << "False!" << std::endl;*/
+        c++;
+        if( c % interval == 0 )
+        {
+            std::cout << '.' << std::flush;
+        }
+    }
+    infile.close();
+    std::cout << "\nFinished inserting all kmers! Processed " << c << " kmers!" << std::endl << std::endl;;
+
+    std::cout << "Verifing kmers were inserted into container";
+    infile = std::ifstream( argv[ 2 ] );
+    c = 0;
+    while( std::getline( infile, line ) )
+    {
+        std::istringstream iss(line);
+        strcpy( kmer, line.substr( 0, k ).c_str() );
+        assert( kd->contains( kmer ) );
 
         c++;
+        if( c % interval == 0 )
+        {
+            std::cout << '.' << std::flush;
+        }
     }
-    std::cout << "Processed " << c << " kmers!" << std::endl;
+    infile.close();
+    std::cout << "\nFinished checking if container contains  all kmers!" << std::endl;
 }
 
 
