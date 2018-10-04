35fdd0f16d616ed10997e7a9a258f8f5cf60f882
Stanley Fujimoto <sfujimoto@gmail.com>
2018-10-03 20:21:57 -0600

    missing header, updated capacity
    updated the default capacity of the kdict for testing purposes with real data

--- src/Container.cc
+++ src/Container.cc
@@ -1,6 +1,6 @@
 #include "Container.h"
 
-int Container::s_capacity = 10;
+int Container::s_capacity = 256;
 int Container::s_prefix_length = 1;
 int Container::s_prfx_prefix_length = 1;
 int Container::s_prfx_suffix_length = 1;
