e7d328e6e280771aa9acefe40bf9abbf898dd263
Stanley Fujimoto <sfujimoto@gmail.com>
2018-10-08 10:28:58 -0600

    remove unnecessary files

--- src/Container.cc
+++
@@ -1,9 +0,0 @@
-#include "Container.h"
-
-uint16_t Container::s_capacity = 256;
-uint8_t Container::s_prefix_length = 1;
-uint8_t Container::s_prfx_prefix_length = 1;
-uint8_t Container::s_prfx_suffix_length = 1;
-
-
-
