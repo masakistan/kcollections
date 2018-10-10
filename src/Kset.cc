a261b71c45214757a5a62c56a355ff66f48c92e6
Cole Lyman <cole@colelyman.com>
2018-10-09 23:50:39 -0600

    Added documentation strings to the python functions

--- src/Kset.cc
+++ src/Kset.cc
@@ -48,15 +48,33 @@ PYBIND11_MODULE( Kcollections, m )
            :toctree: _generate
 
            Kset
-    )pbdoc";
+      )pbdoc";
 
     py::class_<Kset>(m, "Kset")
         .def(py::init<const int>())
-        .def("add", &Kset::insert)
-        .def("__contains__", &Kset::contains)
-        .def("clear", &Kset::clear)
-        .def("__len__", &Kset::size)
-        .def("__delitem__", &Kset::remove);
+        .def("add", &Kset::insert, R"pbdoc(
+            Add a kmer to Kset
+
+            Takes one argument, the kmer represented as a string
+          )pbdoc")
+        .def("__contains__", &Kset::contains, R"pbdoc(
+            Checks if a kmer is in Kset
+
+            Takes one argument, the kmer represented as a string and returns
+            True if kmer is present in Kset or False if it is not present in Kset
+          )pbdoc")
+        .def("clear", &Kset::clear, R"pbdoc(
+            Clears the Kset
+          )pbdoc")
+        .def("__len__", &Kset::size, R"pbdoc(
+            Returns the number of kmers in Kset
+          )pbdoc")
+        .def("__delitem__", &Kset::remove, R"pbdoc(
+            Removes a kmer from Kset
+
+            Takes one argument, the kmer represented as a string and removes it 
+            from the Kset
+          )pbdoc");
 }
 
 
