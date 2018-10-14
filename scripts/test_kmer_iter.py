a4542656fa068d6ccb8ea98e1338aca6858553e5
Stanley Fujimoto <sfujimoto@gmail.com>
2018-10-14 12:54:55 -0600

    update test script

--- scripts/test_kmer_iter.py
+++ scripts/test_kmer_iter.py
@@ -1,61 +1,10 @@
-import sys
-from _Kdict import Kdict as KdictParent
-from _Kset import Kset as KsetParent
+import sys, kcollections
 
-
-class Kdict( KdictParent ):
-    def __init__( self, k ):
-        super( Kdict, self ).__init__( k )
-
-    def __iter__( self ):
-        return self._get_kmers( self.get_root(), self.k )
-
-    def _get_kmers( self, v, k, prefix = '' ):
-        # get from UC
-        for i in range( self.get_uc_size( v ) ):
-            yield prefix + self.get_uc_kmer( v, k, i )
-
-        for i in range( self.get_cc_size( v ) ):
-            for j in range( self.get_cc_child_size( v, i ) ):
-                child_prefix = prefix + self.get_cc_child_suffix( v, i, j )
-                for kmer in self._get_kmers( self.get_cc_child_vertex( v, i, j ), k - 4, child_prefix ):
-                    yield kmer
-
-    def iteritems( self ):
-        for kmer in self._get_kmers( self.get_root(), self.k ):
-            yield ( kmer, self[ kmer ] )
-
-    def keys( self ):
-        for kmer in self._get_kmers( self.get_root(), self.k ):
-            yield kmer
-
-    def values( self ):
-        for kmer in self._get_kmers( self.get_root(), self.k ):
-            yield self[ kmer ]
-
-
-class Kset( KsetParent ):
-    def __init__( self, k ):
-        super( Kset, self ).__init__( k )
-
-    def __iter__( self ):
-        return self._get_kmers( self.get_root(), self.k )
-
-    def _get_kmers( self, v, k, prefix = '' ):
-        # get from UC
-        for i in range( self.get_uc_size( v ) ):
-            yield prefix + self.get_uc_kmer( v, k, i )
-
-        for i in range( self.get_cc_size( v ) ):
-            for j in range( self.get_cc_child_size( v, i ) ):
-                child_prefix = prefix + self.get_cc_child_suffix( v, i, j )
-                for kmer in self._get_kmers( self.get_cc_child_vertex( v, i, j ), k - 4, child_prefix ):
-                    yield kmer
 
 k = 27
-ks = Kset( k )
+ks = kcollections.Kset( k )
 c = 0
-'''with open( sys.argv[ 1 ], 'r' ) as fh:
+with open( sys.argv[ 1 ], 'r' ) as fh:
     for line in fh:
         kmer = line.strip().split()[ 0 ]
         ks.add( kmer )
@@ -70,9 +19,8 @@ for idx, kmer in enumerate( ks ):
     c += 1
 assert c == len( ks )
 print 'Printed a total of', c, 'kmers set!'
-'''
 
-kd = Kdict( k )
+kd = kcollections.Kdict( k )
 c = 0
 with open( sys.argv[ 1 ], 'r' ) as fh:
     for line in fh:
