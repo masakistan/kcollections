2a002bceecfbce847571d483958682b53aa7e0cf
masakistan <sfujimoto@gmail.com>
2018-11-03 17:38:35 -0600

    added python expected methods
    both kset and kdict should have all necessary methods to act as drop-in replacements for set and dict

--- kcollections/__init__.py
+++ kcollections/__init__.py
@@ -20,6 +20,19 @@ class Kdict( KdictParent ):
                 for kmer in self._get_kmers( self.get_cc_child_vertex( v, i, j ), k - 4, child_prefix ):
                     yield kmer
 
+    def __str__( self ):
+        res = []
+        for key, val in self.items():
+            res.append( key + ':' + str( val ) )
+        return '{' + ','.join( res ) + '}'
+
+    def __repr__( self ):
+        return self.__str__()
+
+    def items( self ):
+        for kmer in self._get_kmers( self.get_root(), self.k ):
+            yield ( kmer, self[ kmer ] )
+
     def iteritems( self ):
         for kmer in self._get_kmers( self.get_root(), self.k ):
             yield ( kmer, self[ kmer ] )
@@ -31,6 +44,52 @@ class Kdict( KdictParent ):
     def values( self ):
         for kmer in self._get_kmers( self.get_root(), self.k ):
             yield self[ kmer ]
+
+    def copy( self ):
+        new_kdict = Kdict( self.k )
+        for kmer in self:
+            new_kdict[ kmer ] = self[ kmer ]
+        return new_kdict
+
+    def get( self, key, value = None ):
+        if key in self:
+            return self[ key ]
+        else:
+            return value
+
+    def popitem( self ):
+        kmer, item = next( self.items() )
+        del self[ kmer ]
+        return ( kmer, item )
+
+    def setdefault( self, key, value = None ):
+        if key not in self:
+            self[ key ] = value
+            return value
+        else:
+            return self[ key ]
+
+    def pop( self, key, *default ):
+        if key in self:
+            value = self[ key ]
+            del self[ key ]
+            return value
+        else:
+            if len( default ) > 0:
+                return default
+            else:
+                raise KeyError( key )
+
+    def update( self, *others ):
+        for other in others:
+            for item in other:
+                try:
+                    key, val = item
+                except:
+                    key = item
+                    val = other[ key ]
+                self[ key ] = val
+                
 
 
 class Kset( KsetParent ):
@@ -51,3 +110,117 @@ class Kset( KsetParent ):
                 for kmer in self._get_kmers( self.get_cc_child_vertex( v, i, j ), k - 4, child_prefix ):
                     yield kmer
 
+    def __str__( self ):
+        return '{' + ','.join( self ) + '}'
+
+    def __repr( self ):
+        return self.__str__()
+
+    def copy( self ):
+        new_set = Kset( self.k )
+        for kmer in self:
+            new_set.add( kmer )
+        return new_set
+
+    def update( self, *iters ):
+        for _iter in iters:
+            for item in _iter:
+                self.add( item )
+        return self
+
+    def discard( self, item ):
+        if item in self:
+            del self[ item ]
+
+    def pop( self ):
+        kmer = next( self._get_kmers( self.get_root(), self.k ) )
+        del self[ kmer ]
+        return kmer
+
+    def isdisjoint( self, other ):
+        for kmer in other:
+            if kmer in self:
+                return False
+        return True
+
+    def issubset( self, other ):
+        for kmer in self:
+            if kmer not in other:
+                return False
+        return True
+
+    def issuperset( self, other ):
+        for kmer in other:
+            if kmer not in self:
+                return False
+        return True
+
+    def intersection( self, *other_sets ):
+        new_set = Kset( self.k )
+        for kmer in self:
+            good = True
+            for other_set in other_sets:
+                if kmer not in other_set:
+                    good = False
+                    break
+            if good:
+                new_set.add( kmer )
+        return new_set
+
+    def intersection_update( self, *other_sets ):
+        self = self.intersection( *other_sets )
+        return self
+
+    def difference( self, other ):
+        new_set = Kset( self.k )
+        for kmer in self:
+            if kmer not in other:
+                new_set.add( kmer )
+        return new_set
+
+    def difference_update( self, *other_sets ):
+        for other_set in other_sets:
+            for kmer in other_set:
+                self.discard( kmer )
+        return self
+
+    def symmetric_difference( self, other_set ):
+        new_set = Kset( self.k )
+        for kmer in self.__iter__():
+            if kmer not in new_set:
+                if (
+                        ( kmer not in self and kmer in other_set )
+                        or ( kmer in self and kmer not in other_set )
+                        ):
+                    new_set.add( kmer )
+        for kmer in other_set:
+            if kmer not in new_set:
+                if(
+                        ( kmer not in self and kmer in other_set )
+                        or (kmer in self and kmer not in other_set )
+                        ):
+                    new_set.add( kmer )
+        return new_set
+
+    def symmetric_difference_update( self, other_set ):
+        self = self.symmetric_difference( other_set )
+        return self
+
+    def union( self, *other_sets ):
+        new_set = Kset( self.k )
+        for other_set in other_sets:
+            for kmer in other_set:
+                new_set.add( kmer )
+        return new_set
+
+    def __and__( self, other ):
+        return self.intersection( other )
+
+    def __or__( self, other ):
+        return self.update( other )
+
+    def __xor__( self, other ):
+        return self.symmetric_difference( other )
+
+    def __sub__( self, other ):
+        return self.difference( other )
