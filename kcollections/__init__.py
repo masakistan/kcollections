5d30d2895730e04c1076c555ab82e807b1e3ecb8
Stanley Fujimoto <sfujimoto@gmail.com>
2018-12-18 19:37:50 -0700

    update python bindings

--- kcollections/__init__.py
+++ kcollections/__init__.py
@@ -1,6 +1,95 @@
 from ._Kdict import Kdict as KdictParent
 from ._Kset import Kset as KsetParent
 from ._Kcounter import Kcounter as KcounterParent
+from ._Kcolor import Kcolor as KcolorParent
+
+
+class Kcolor( KcolorParent ):
+    def __init__( self, k ):
+        super( Kcolor, self ).__init__( k )
+
+    def __iter__( self ):
+        return self._get_kmers( self.get_root(), self.k )
+
+    def _get_kmers( self, v, k, prefix = '' ):
+        # get from UC
+        for i in range( self.get_uc_size( v ) ):
+            yield prefix + self.get_uc_kmer( v, k, i )
+
+        for i in range( self.get_vs_size( v ) ):
+            child_prefix = prefix + self.get_child_suffix( v, i )
+            for kmer in self._get_kmers( self.get_child_vertex( v, i ), k - 4, child_prefix ):
+                yield kmer
+
+    def __str__( self ):
+        res = []
+        for key, val in self.items():
+            res.append( key + ':' + str( val ) )
+        return '{' + ','.join( res ) + '}'
+
+    def __repr__( self ):
+        return self.__str__()
+
+    def items( self ):
+        for kmer in self._get_kmers( self.get_root(), self.k ):
+            yield ( kmer, self[ kmer ] )
+
+    def iteritems( self ):
+        for kmer in self._get_kmers( self.get_root(), self.k ):
+            yield ( kmer, self[ kmer ] )
+
+    def keys( self ):
+        for kmer in self._get_kmers( self.get_root(), self.k ):
+            yield kmer
+
+    def values( self ):
+        for kmer in self._get_kmers( self.get_root(), self.k ):
+            yield self[ kmer ]
+
+    def copy( self ):
+        new_kcolor = Kcolor( self.k )
+        for kmer in self:
+            new_kcolor[ kmer ] = self[ kmer ]
+        return new_kcolor
+
+    def get( self, key, value = None ):
+        if key in self:
+            return self[ key ]
+        else:
+            return value
+
+    def popitem( self ):
+        kmer, item = next( self.items() )
+        del self[ kmer ]
+        return ( kmer, item )
+
+    def setdefault( self, key, value = None ):
+        if key not in self:
+            self[ key ] = value
+            return value
+        else:
+            return self[ key ]
+
+    def pop( self, key, *default ):
+        if key in self:
+            value = self[ key ]
+            del self[ key ]
+            return value
+        else:
+            if len( default ) > 0:
+                return default
+            else:
+                raise KeyError( key )
+
+    def update( self, *others ):
+        for other in others:
+            for item in other:
+                try:
+                    key, val = item
+                except:
+                    key = item
+                    val = other[ key ]
+                self[ key ] = val
 
 
 class Kdict( KdictParent ):
